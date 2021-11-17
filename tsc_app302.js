20c96e7049c6585c154749e705d2c9001fd50bed
patipan-patisampita <patipan.patisampita@gmail.com>
2021-11-17 12:56:31 +0700

    update v1

---
+++ tsc_app302.js
@@ -0,0 +1,4 @@
+var a = '1500000'; //Javascript type : dynamically type
+var b = 'Welcome to TypeScript'; //TypeScript type: Statically Type
+console.log(typeof a);
+console.log(typeof b);
---
+++ tsc_app302.ts
@@ -0,0 +1,4 @@
+let a = '1500000' //Javascript type : dynamically type
+let b: string = 'Welcome to TypeScript' //TypeScript type: Statically Type
+console.log(typeof a)
+console.log(typeof b)
